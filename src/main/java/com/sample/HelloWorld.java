3b5978367f0faf6cf9e40f0d228a28d24cd601a2
pracity <tathode@gmail.com>
2016-12-01 22:24:15 +0000

    test changes saved

--- src/main/java/com/sample/HelloWorld.java
+++ src/main/java/com/sample/HelloWorld.java
@@ -1,17 +1,21 @@
 package com.sample;
 
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/helloWorld")
 public class HelloWorld {
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public String hello(ModelMap model) {
-		model.addAttribute("msg", "JCG Hello World!");
-		return "helloWorld";
-	}
-
+	@RequestMapping("/greeting")
+    public Greeting greeting(Model model) {
+		return new Greeting(1, "abc"); 
+    }
 }
---
+++ src/main/java/com/sample/RESTConfiguration.java
@@ -0,0 +1,24 @@
+package com.sample;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configurable
+public class RESTConfiguration {
+	
+	@Bean
+    public View jsonTemplate() {
+        MappingJackson2JsonView view = new MappingJackson2JsonView();
+        view.setPrettyPrint(true);
+        return view;
+    }
+     
+    @Bean
+    public ViewResolver viewResolver() {
+        return new BeanNameViewResolver();
+    }
+}
